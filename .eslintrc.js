bb2b2e6c9b9db72b3396eb680246cda3111896cb
Nik Cimino <nik@royalprotocol.io>
2022-02-27 11:14:29 -0700

    fixes all but one lint error, but perhaps we can just remove the linter stuff all together

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,35 @@
 module.exports = {
+  rules: {
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      { ignores: ["modules", "import"] },
+    ],
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+  },
   env: {
     browser: false,
     es2021: true,
     mocha: true,
     node: true,
+    jest: true,
   },
   extends: [
     "standard",
+    // "plugin:react/recommended",
     "plugin:prettier/recommended",
-    "plugin:node/recommended",
+    // "plugin:node/recommended",
   ],
+  plugins: ["react"],
   parserOptions: {
     ecmaVersion: 12,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   overrides: [
     {
